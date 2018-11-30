5e4c363774df1eb7356ded0c32594687fc605602
mhaya <masaharu.hayashi3@gmail.com>
2018-11-30 14:02:44 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+coverage run manage.py test
+coverage report
+python manage.py test
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-'django_nose',
+    'django_nose',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
